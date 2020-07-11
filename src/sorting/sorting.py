7842e00dbc37868b47bc850a6a477037bacfdfe0
Ian <mus5002@aol.com>
2020-07-11 11:22:40 -0400

    merge sort passing all tests

--- src/sorting/sorting.py
+++ src/sorting/sorting.py
@@ -11,37 +11,22 @@ def merge(arrA, arrB):
     while index_a < len(arrA) and index_b < len(arrB):
         # compare elements at given index
         # set smaller value at given index of merged_arr
-        print('\n')
-        print(f'index: {index_a}, {index_b}')
-        print(f'A:{arrA} || B:{arrB}')
-        print(f'{arrA[index_a]} : {arrB[index_b]}')
         if arrA[index_a] < arrB[index_b]:
             merged_arr.append(arrA[index_a])
-            print(merged_arr)
-            print(index_a)
             index_a += 1
-            print(index_a)
         else:
             merged_arr.append(arrB[index_b])
-            print(merged_arr)
             index_b += 1
-    if index_a == len(arrA):
-        merged_arr.extend(arrB[index_b:])
-    else:
-        merged_arr.extend(arrA)
+    merged_arr.extend(arrB[index_b:])        
+    merged_arr.extend(arrA[index_a:])
     return merged_arr
 
 # TO-DO: implement the Merge Sort function below recursively
 def merge_sort(arr):
     # Your code here
     if len(arr) <= 1:
-        print('\n')
-        print(arr)
         return arr
-    print('\n')
-    print(arr)
     mid = int(len(arr)/2)
-    print(mid)
     # split array and call merge_sort on each half
     left = merge_sort(arr[:mid])
     right = merge_sort(arr[mid:])
@@ -59,12 +44,3 @@ def merge_in_place(arr, start, mid, end):
 def merge_sort_in_place(arr, l, r):
     # Your code here
     return
-
-# array1 = [4,2,6]
-# array2 = [7,3,1]
-# result = merge(array1, array2)
-
-# appended = array1 + array2
-# result = merge_sort(appended)
-
-# print(result)
